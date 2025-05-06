58bb3c96f2fdb508f417539360de33e2050548e2
margoxt <angelicatinio14@gmail.com>
2025-05-06 18:17:49 +0800

    feat: Added a model folder and workout file to define the schema and model of the documents

---
+++ backend/models/workout.js
@@ -0,0 +1,28 @@
+//The purpose of this file is to define how the workout documents should look
+
+//Require mongoos because its the one that allows us to create schemas and connect to the database. Mongodb alone is schema
+const mongoose = require('mongoose')
+
+//Create schema
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({  //Pass in an argument that we define the schema
+    //First Argument: Describes how the pbject looks
+    title: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    load:{
+        type: Number,
+        required: true
+    }
+    //Second Argument: when we add a new object the program will automatically do a timestamp, to say when the document was created
+}, {timestamps: true})
+
+
+//Model based on the Schema; apply the schema to a model and we use the model to interact with the collection of that name
+module.export = mongoose.model('Workout', workoutSchema)
