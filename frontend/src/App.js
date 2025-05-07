d800c98ef7572f6263cb635c36a8228c49d1f72e
margoxt <angelicatinio14@gmail.com>
2025-05-07 15:54:59 +0800

    feat: created frontend with react and installed the dom

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*Notes:
+- Install react-router-dom so we can have multiple pages to this application
+- npm install react-router-dom 
+*/
