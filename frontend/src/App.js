4dc85696e3c4cc752a399d09ab4a1cfdf27af017
margoxt <angelicatinio14@gmail.com>
2025-05-07 16:36:33 +0800

    feat: created navbar.js and home.js, edited the app.js and index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//Pages & Components
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <div className="pages">
-          
+          <Routes>
+            <Route                        // Needs 2 different props
+              path="/"                    // one slash means the default or the whole page
+              element={<Home/>}           // element refers to what we want to render
+            />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+//Import link to enable navigation of multiple pages
+import { Link } from 'react-router-dom';
+
+const Navbar = () =>{
+    return(
+        <header>
+            <div className="container">
+                <Link to="/">
+                <h1>Workout Buddy</h1>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar;
