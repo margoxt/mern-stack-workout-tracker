b6e3461e008ac2e2cc0e34ea834cf7d287a3731d
margoxt <angelicatinio14@gmail.com>
2025-05-09 17:05:07 +0800

    feat: created a delete function for each workout

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -9,6 +9,8 @@ export const workoutsReducer = (state, action) =>{
             return {workouts: action.payload}
         case 'CREATE_WORKOUT':
             return{workouts: [action.payload, ...state.workouts]}
+        case 'DELETE_WORKOUT':
+            return{workouts: state.workouts.filter((w) => w._id !==action.payload._id)}
         default:
             return state
     }
