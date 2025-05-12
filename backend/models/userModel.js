a4b716a702f4e78b8d86c63835e1e6fa39206cb7
margoxt <angelicatinio14@gmail.com>
2025-05-12 19:11:52 +0800

    feat: created password and email validator

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 //User Schema, defining the structure of the database
 const userSchema = new Schema({
@@ -17,6 +18,20 @@ const userSchema = new Schema({
 
 //Hash Passwords by using a static signup method
 userSchema.statics.signup = async function(email, password){    //Use the function method, not the arrow method, because we are using the "this" keyword
+    
+    //Validation: did they enter an email or password?
+    if(!email || !password){
+        throw Error('All fields must be filled.')
+    }
+    //Validation: is the email valid?
+    if(!validator.isEmail(email)){
+        throw Error('Email is not valid.')
+    }
+    //Check if the password is strong enough
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough.')
+    }
+    
     const exists = await this.findOne({email})      //Use the word this to refer to this file, because the User export is at the bottom.
     
     //If the user already exists, return an error
