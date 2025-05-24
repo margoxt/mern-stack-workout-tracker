c427a020dc37a07b3e9ceba24d69d9da65b94f0a
margoxt <angelicatinio14@gmail.com>
2025-05-24 20:28:51 +0800

    feat: clearing the global states when logging out

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext"
+import { useWorkoutsContext } from "./useWorkoutsContext"
 
 export const useLogout = () =>{
     const { dispatch } = useAuthContext()
+    const { dispatch: workoutsDispatch} = useWorkoutsContext()
 
     const logout = () =>{
         //Remove User from storage
@@ -9,6 +11,7 @@ export const useLogout = () =>{
 
         //Dispatch Logout Function, just reset the user to be null, no payload like in the Auth Context
         dispatch({type: 'LOGOUT'})
+        workoutsDispatch({type: 'SET_WORKOUTS', payload: null}) //clear the global workouts state when logging out
     }
 
     return {logout}
